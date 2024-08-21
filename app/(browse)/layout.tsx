6e6fa1054d51496f33b51a00363608625b461483
padi-dev-manhtd <manhtd@paditech.com>
2024-08-21 08:54:53 +0700

    recommended list user

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,13 +1,16 @@
 import Container from "@/app/(browse)/_components/Container";
 import Navbar from "@/app/(browse)/_components/navbar";
-import Sidebar from "@/app/(browse)/_components/sidebar";
+import Sidebar, { SidebarSkeleton } from "@/app/(browse)/_components/sidebar";
+import { Suspense } from "react";
 
 const BrowerLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
       <div className="flex h-full pt-20">
-        <Sidebar />
+        <Suspense fallback={<SidebarSkeleton />}>
+          <Sidebar />
+        </Suspense>
         <Container>{children}</Container>
       </div>
     </>
